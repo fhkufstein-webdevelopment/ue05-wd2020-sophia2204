4596290dbacb77a53032a3986d28d35870308035
Daniel Hoover <danielhoover@users.noreply.github.com>
2020-03-17 19:25:31 +0100

    Initial commit

---
+++ js/user.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+
+    var userListBody = $('.userList tbody');
+
+    //@todo store and somehow update the current number of users
+
+
+    $('.needs-validation').submit(function(event) {
+
+        event.preventDefault();
+        event.stopPropagation();
+
+        if (this.checkValidity() === false) {
+
+            $(this).addClass('was-validated');
+
+            return false;
+        }
+
+        //@todo
+        //1. get values
+        //2. create a new element
+        //3. somehow add them to userListBody
+        //4. update number of current users
+        //5. clear entries from the form
+        //6. maybe do something else... :-)
+
+        //your code follows here
+
+        return false;
+    });
+
+
+    $('.deleteTrigger').click(function() {
+        //@todo
+        //1. remove current user from dom
+        //2. update number of current users
+
+        //your code follows here
+
+
+    });
+
+    //maybe some code follows here
+
+});
