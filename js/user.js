439a5aa9359bc54524597c5366869813f2bb1d8e
Sophia Habersatter <1910653396@stud.fh-kufstein.ac.at>
2020-03-19 19:04:12 +0100

    aufgabe 2

--- js/user.js
+++ js/user.js
@@ -1,19 +1,17 @@
 $(document).ready(function() {
 
     var userListBody = $('.userList tbody');
+    var userNumber = 1;
 
     //@todo store and somehow update the current number of users
 
 
     $('.needs-validation').submit(function(event) {
-
         event.preventDefault();
         event.stopPropagation();
 
         if (this.checkValidity() === false) {
-
             $(this).addClass('was-validated');
-
             return false;
         }
 
@@ -27,20 +25,47 @@ $(document).ready(function() {
 
         //your code follows here
 
+        userNumber++;
+        var newUser = document.getElementById('username').value;
+        userListBody.push(newUser);
+
+        var table = document.getElementById('UserTable');
+        var newRow = table.insertRow(-1);
+        var cell1 = newRow.insertCell(0);
+        var cell2 = newRow.insertCell(1);
+        var cell3 = newRow.insertCell(2);
+
+        cell1.innerHTML = userNumber;
+        cell2.innerHTML = newUser;
+        cell3.innerHTML = "<button type=\"button\" class=\"btn btn-secondary btn-danger deleteTrigger\" title=\"Löschen\"><i class=\"fa fa-trash\"></i></button>";
+
+        document.getElementById("form").reset();
+
         return false;
     });
 
 
-    $('.deleteTrigger').click(function() {
+    $(document).on('click', '.deleteTrigger', function() {
         //@todo
         //1. remove current user from dom
         //2. update number of current users
 
         //your code follows here
 
+        var table = document.getElementById("UserTable");
+        var index = document.getElementById("position").value;
+        var length = userListBody.length;
+        var row = table.deleteRow(length);
+
+        //optional
+
+        if(confirm("Do you really want to delete this user?")) {
+            $(this).parents("tr").remove();
+            userNumber--
+        } else {
+            console.log("Cancelled.");
+        }
 
     });
-
-    //maybe some code follows here
 
 });
